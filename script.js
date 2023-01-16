e57f1340fcc704797c31aa866c0240c7f52da700
manuellpz <manuelmzt44@gmail.com>
2023-01-16 12:39:01 -0700

    funcionalidad

---
+++ script.js
@@ -0,0 +1,33 @@
+const container = document.querySelector(".container");
+const txtItem = document.querySelector("#txt_item");
+
+addEventListener('load',()=>{showItems()});
+
+txtItem.addEventListener("keyup", (e) => {
+  if (e.keyCode === 13) {
+    addItem(e.target.value);
+    showItems();
+  }
+});
+
+function showItems() {
+  container.innerHTML = '';
+  let template = "";
+  let totalItems = localStorage.length;
+  let item = "";
+  for (i = 0; i < totalItems; i++) {
+    item = localStorage.getItem(`${i}`);
+    template = `
+         <div class="container_item" id="item_${i}">
+            <p>${item}</p>
+            <button class="btn_delete">X</button>
+         </div>
+      `;
+    container.innerHTML += template;
+  }
+}
+
+function addItem(item) {
+  let counter = localStorage.length;
+  localStorage.setItem(counter, item);
+}
